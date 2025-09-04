514e6bb921ae4b9b4b3ecebad50f724e1505e060
Name <user@example.com>
2025-09-04 09:44:07 +0530

    final updates

--- chasmawala-backend/testBcrypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcrypt");
-
-const inputPassword = "virat123"; // Password user entered at login
-const storedHash = "$2b$10$rT.vNjGpebjvQsOGTBJ7XeSiqeL7PnkCUDVuMpQ6iK/inuXN.mrki"; // Your stored hash from MongoDB
-
-bcrypt.compare(inputPassword, storedHash, (err, isMatch) => {
-    console.log("🔍 Password Match Test:", isMatch ? "✅ Match" : "❌ No Match");
-});
